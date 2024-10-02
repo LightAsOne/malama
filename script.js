a983565043de81a2a3b09b6ebf99b69f276c0cc0
LightAsOne <shine@lightasone.com>
2024-10-02 20:16:12 +1000

    script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const countdownTimer = setInterval(() => {
 // Video Configuration
 let videos = [
     "https://www.youtube.com/embed/jlXVFzqLQ0s", // Day 1 Video
-    "https://youtube.com/shorts/vx8yoBPKBs4?feature=share",
+    "https://www.youtube.com/shorts/vx8yoBPKBs4",
 	
     // Add more video links as days progress
 ];
